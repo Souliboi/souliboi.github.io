7f5a67a61c935bea80a21d54d01c09388a72d45e
Souliboi <souliboi@proton.me>
2024-04-18 20:08:29 +0200

    finally a bigger update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var creditModalJs = document.getElementById("credit-modal-id");
-var creditBtn = document.getElementById("credit-id");
-var creditExit = document.getElementsByClassName("credit-close")[0];
+const creditModalJs = document.getElementById("credit-modal-id");
+const creditBtn = document.getElementById("credit-id");
+const creditExit = document.getElementsByClassName("credit-close")[0];
 
 creditBtn.onclick = function() {
   creditModalJs.style.display = "block";
@@ -11,7 +11,21 @@ creditExit.onclick = function() {
 }
 
 window.onclick = function(event) {
-  if (event.target == creditModalJs) {
+  if (event.target === creditModalJs) {
     creditModalJs.style.display = "none";
+  } else if (event.target === wikiModalJs) {
+    wikiModalJs.style.display = "none";
   }
+}
+
+const wikiModalJs = document.getElementById("wiki-db-modal-id");
+const wikiBtn = document.getElementById("wiki-db-id");
+const wikiExit = document.getElementsByClassName("wiki-db-close")[0];
+
+wikiBtn.onclick = function() {
+  wikiModalJs.style.display = "block";
+}
+
+wikiExit.onclick = function() {
+  wikiModalJs.style.display = "none";
 }
